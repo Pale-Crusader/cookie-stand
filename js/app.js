7f56d29827a4611790b50d613b323605624be446
MatthewStewart <matthew.adam.stewart@gmail.com>
2020-03-13 07:24:49 -0700

    class 11 making table display

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'strict';
 var listOfBranches =[];
+var listOfHours =['6 am','7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm'];
 
 // name is a string of the name, minCust is minimum _number_ of customers, maxCust is maximum _number_ of customers, branchID is the HTML parent ID
 function StoreBranch(name, minCust, maxCust, avgCookie, branchID) {
@@ -11,7 +12,7 @@ function StoreBranch(name, minCust, maxCust, avgCookie, branchID) {
 	listOfBranches.push(this);
 }
 
-StoreBranch.prototype.numberOfHoursOpen = ['6 am','7 am','8 am','9 am','10 am','11 am','12 pm','1 pm','2 pm','3 pm','4 pm','5 pm','6 pm','7 pm'];
+StoreBranch.prototype.numberOfHoursOpen = listOfHours;
 StoreBranch.prototype.customersEachHourArray = [];
 StoreBranch.prototype.cookiesEachHourArray = [];
 StoreBranch.prototype.runningCookieTotal = 0;
@@ -32,7 +33,7 @@ StoreBranch.prototype.calculateAllDaySales = function() {
 		this.runningCustomerTotal = (this.runningCustomerTotal + branchHourlyArray[0]);
 		this.cookiesEachHourArray.push(branchHourlyArray[1]);
 		this.runningCookieTotal = (this.runningCookieTotal + branchHourlyArray[1]);
-		console.log('This ' + this.name + '\'s cookiesEachHourArray is ' + this.cookiesEachHourArray + ' at index number: '+ index + ' and this is what was added this loop: ' + branchHourlyArray[1]);
+		// console.log('This ' + this.name + '\'s cookiesEachHourArray is ' + this.cookiesEachHourArray + ' at index number: '+ index + ' and this is what was added this loop: ' + branchHourlyArray[1]);
 
 	switch (index) {
 		case 0:
@@ -104,7 +105,7 @@ StoreBranch.prototype.calculateAllDaySales = function() {
 		case 13:
 			var fourteenthHour = document.createElement('td');
 			fourteenthHour.textContent = (' ' + branchHourlyArray[1]);
-			branchParent.appendChild(tenthHour);
+			branchParent.appendChild(fourteenthHour);
 			var displayTotalElement = document.createElement('td');
 			displayTotalElement.textContent = (' ' + this.runningCookieTotal);
 			branchParent.appendChild(displayTotalElement);
@@ -114,12 +115,55 @@ StoreBranch.prototype.calculateAllDaySales = function() {
 		}
 	}
 }
+// function displayColumnTotal(listOfBranches) {
+	
+// 	var columnTotalingArray = listOfBranches;
+// 	vat displayColumnTotalElement = document.getElementById("displayColummTotals")
+
+// 	for (var eachHour = 0; listOfHours.length; 
+// 		var columnRunningSum = 0;
+// 		for (var columnIndex = 0; columnIndex < listOfBranches.length; columnIndex++) {
+// 			ru
+			
+		
+// 	var columnTotalEl = document.createElement('td');
+// 	columnTotalEl.textContent = (' ' + this.runningCookieTotal);
+// 	branchParent.appendChild(displayTotalElement);
+// 	}
+
+// }
 
 var seattleBranch = new StoreBranch('Seattle', 23, 65, 6.3, 'branch1');
 var tokyoBranch = new StoreBranch('Tokyo', 3, 24, 1.2, 'branch2');
 var dubaiBranch = new StoreBranch('Dubai', 11, 38, 3.7, 'branch3');
 var parisBranch = new StoreBranch('Paris', 20, 38, 2.3, 'branch4');
 var limaBranch = new StoreBranch('Lima', 2, 16, 4.6, 'branch5');
+
+
+// function handleForm(event) {
+// 	event.preventDefault();
+// 	var salesTable = document.getElementById('sales');
+// 	if (name !== '' && avgCookie > 0 && maxCust > minCust) {
+// 		var name = event.target.name.value;
+// 		var minCust = event.target.minCust.value;
+// 		var maxCust = event.target.maxCust.value;
+// 		var avgCookie = event.target.avgCookie.value;
+// 		var branchID = ('branch' + ( listOfBranches.length + 2 ));
+// 		var newBranch = new StoreBranch(name, minCust, maxCust, branchID);
+// 		console.log(newBranch);
+// 		var newRowParent = document.createElement('tr');
+// 		var dynamicRowID = document.createAttribute('id');
+// 		dynamicRowID.value = branchID;
+// 		newRowParent.setAttributeNode(dynamicRowID);
+// 		salesTable.appendChild(newRowParent);
+// 		var newTitleCell = document.createElement('td');
+// 		newTitle.textContent = (' ' + name);
+// 		newRowParent.appendChild(newTitleCell);
+// 		newBranch.calculateAllDaySales();
+// 	} else {
+// 		alert('Please make sure the form is filled out with name and appropriate numerical value.');
+// 	}
+// }
 
 console.log(seattleBranch);
 console.log(tokyoBranch);
@@ -131,3 +175,8 @@ tokyoBranch.calculateAllDaySales();
 dubaiBranch.calculateAllDaySales();
 parisBranch.calculateAllDaySales();
 limaBranch.calculateAllDaySales();
+
+
+// var createNewStoreForm = document.getElementById('makeBranchForm');
+
+// createNewStoreForm.addEventListener('submit', handleForm);
